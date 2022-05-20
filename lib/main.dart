a2461de48a3752d93befd3b1518bf4c4038e7e58
michaelangelofrancisco <mafgwaf@gmail.com>
2022-05-21 05:49:50 +0800

    design

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:retail_store_management_system/routes/Home.dart';
-import 'package:retail_store_management_system/routes/login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Retail Store Management System',
+      theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(
+        Theme.of(context).textTheme,
+      )),
+      title: '',
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => Login(),
-        '/home': (context) => const Home(),
-      },
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: ListView(
+        children: [
+          Menu();
+        ],
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _menuItem(title: 'Home'),
+        ],
+      ),
+    ); 
+  }
+  Widget _menuItem({String title, bool isActive = false}){
+    return Padding(
+      padding: EdgeInsets.only(right: 75),
+      child: Column(
+        children: [
+          Text('$title', style: TextStyle(fontWeight: FontWeight.bold, color))
+        ],
+      ),
     );
   }
 }
